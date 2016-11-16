d4bd32ffbbc2a3ff34c8fbea5555d3d66816efb0
Rosie <r.gollancz@gmail.com>
2016-11-16 10:53:23 +0000

    Adds user_id to restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115141037) do
+ActiveRecord::Schema.define(version: 20161116105205) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20161115141037) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "location"
+    t.integer  "user_id"
   end
+
+  add_index "restaurants", ["user_id"], name: "index_restaurants_on_user_id"
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
