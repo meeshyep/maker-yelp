182782aa9979ff701917ae4e5da9eddc43d190a0
Anna Beddow <anna.beddow@gmail.com>
2016-11-16 17:25:36 +0000

    removed obselete tests

--- spec/views/users/_login_items.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "restaurants/_login_items.html.erb", type: :view do
-  scenario "user can login" do
-    visit 'restaurants/new'
-    expect(page).to have_content("Add restaurant")
-  end
-end
