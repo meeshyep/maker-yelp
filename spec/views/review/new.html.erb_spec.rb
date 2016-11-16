a8dfc8056be093e2ffe01e17f940c0fccff141dc
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-11-16 16:21:07 +0000

    even better code

--- spec/views/review/new.html.erb_spec.rb
+++ spec/views/review/new.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "review/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  feature "can add a review" do
+    scenario "user can add review to restaurat" do
+    end
+  end
+    scenario "user cannot add review to restaurant that they have already reviewed" do
+  end
 end
